9a2ed6d1a46222267d6da4d0cfe684ef614199d5
luanmvcosta0 <luanmvcosta@gmail.com>
2025-02-14 14:27:50 -0300

    first commit

---
+++ src/test/java/com/login_page/FullStackApiLoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackApiLoginPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
